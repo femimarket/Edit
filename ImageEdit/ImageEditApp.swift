41db3cead269d3b23ad4a34502dc832f2f4981b1
femimarket <BJTDhwzXnYx7TLwQdyhVoArwbQnNAMJj2gq5Zs5hpump@femi.market>
2026-06-22 20:00:06 +0100

    Initial Commit

---
+++ ImageEdit/ImageEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditApp.swift
+//  ImageEdit
+//
+//  Created by u on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
